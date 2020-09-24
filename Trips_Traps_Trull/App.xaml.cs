297bb7b79a8f817076848d896a7e1301e4a5a282
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2020-09-24 12:11:11 +0300

    Add project files.

---
+++ Trips_Traps_Trull/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Trips_Traps_Trull
+{
+    public partial class App : Application
+    {
+        public bool first_player=false;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            
+            
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
